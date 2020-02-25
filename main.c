a7b022c09316e2c6e285f2010ce34a654550ff17
Matthew Boland <c3355721@uon.edu.au>
2020-02-25 14:24:08 +1100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float x;
+    float y;
+    printf("Enter an integer: ");
+    scanf("%f", &x);
+    y = x*x*x;
+    printf("y: %f\n", y);
+    return 0;
+    }
